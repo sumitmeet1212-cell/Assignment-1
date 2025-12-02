2f0d3d636bf3cb031e9bb2834ade78f93f59c7d2
Sumit Kumar <sumitmeet1212@gmail.com>
2025-12-02 20:57:46 +0530

    Add files via upload

---
+++ mathematical_operations.py
@@ -0,0 +1,10 @@
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+add=num1+num2
+sub=num1-num2
+mul=num1*num2
+div=num1/num2
+print(f"Addition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")
